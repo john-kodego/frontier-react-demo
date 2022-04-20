5bddecb5432eba11e05cb1cfddd7af153b787a62
john-kodego <john@kodego.ph>
2022-04-20 09:19:10 +0800

    [*] dexter's comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// dexter's comment
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
